666f036dbd23499883333e6dae8f5e822b6037ea
BugHunter-X <askzenai@gmail.com>
2025-01-15 18:12:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+function checkNumber(num) {
+  if (num > 0) {
+    return "positive";
+  } else if (num < 0) {
+    return "negative";
+  }
+  // Missing else statement to handle the case when num is 0
+}
